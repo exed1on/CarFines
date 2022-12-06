e310b3aa0a531dab77b2598804334916ee93de61
exed1on <exed1on.uni@gmail.com>
2022-12-06 06:56:15 +0100

    with home page

--- src/main/java/com/example/lastdockertest/controllers/HomeController.java
+++ src/main/java/com/example/lastdockertest/controllers/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SubController {
-    @GetMapping(value = "/view")
+public class HomeController {
+    @GetMapping(value = "/")
     public String home(Model model)
     {
         model.addAttribute("title","Sign In");
-        return "view";
+        return "index";
     }
 }
